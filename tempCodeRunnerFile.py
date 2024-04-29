54748c421cf8c39ccaa1447c579b902c66075ca1
ktoan911 <khanhtoan.forwork@gmail.com>
2024-04-29 17:43:07 +0700

    update gitignore

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-max_input_length = 100
-    max_target_length = 100
-    batch_size = 64
-    train_dataset, val_dataset, test_dataset, input_tokenizer, target_tokenizer = dataset.data_process(
-        max_input_length, max_target_length, batch_size)
-    print('done')
-    print(train_dataset)
